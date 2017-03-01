5684e664ab0717c6100c0dac554c85a52e756e32
piflynn <piflynn@me.com>
2017-03-01 07:33:31 -0500

    Fix api call. add signature.

--- js/script.js
+++ js/script.js
@@ -1,26 +1,36 @@
 $(document).ready(function() {
-  var search = '';
-  var results = '';
+  
 
   function wikiSearch() {
     search = $("#search").val();
     results = '';
 
-    $.getJSON("http://en.wikipedia.org/w/api.php?action=opensearch&search="+search+"&limit=10&namespace=0&format=json&callback=?",  function(json) {
-
-    for(var i = 0; i < json[1].length; i++) {
-      results += '<a href="'+json[3][i]+'" class="list-group-item list-group-item-info"><h4><b>'+json[1][i]+'</b></h4><p>'+json[2][i]+'</p></a>'
-    }
-
-    $("#results").html(results);
-
+    
+    $.ajax({
+        type: "GET",
+        url: "http://en.wikipedia.org/w/api.php?action=query&list=search&srsearch="+search+"&format=json&callback=?",
+        origin: "https://piflynn.github.io/wikizi",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json"
+    }).done(function(json) {
+      if(json.query !== undefined) {
+        for(var i = 0; i < json.query.search.length; i++) {
+          results += '<a href="https://en.wikipedia.org/wiki/'+ json.query.search[i].title +'" class="list-group-item list-group-item-info"><h4><b>'+ json.query.search[i].title+'</b></h4><p>'+json.query.search[i].snippet+'</p></a>';
+        }
+      }
+      else {
+        results = '';
+      }
+      $("#results").html(results);
     });
+    
+
+
   }
 
   $("#search").keyup(wikiSearch);
   $("#go").click(wikiSearch);
-
  
   
 });
-      
+
